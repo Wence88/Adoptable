5b6b5ff784051a5fec9388ca211e7b08dab10818
Oumide Ajidahun <wenceslausajidahun@gmail.com>
2024-03-12 14:10:22 +0100

    final work

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+export default App;
